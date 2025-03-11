26283c02ee7bdcb7c4f7db0d769d6ecb9c5eede9
ratulislam46 <arfanratul46@gmail.com>
2025-03-11 23:59:35 +0600

    increment concept and example

---
+++ js/increment.js
@@ -0,0 +1,14 @@
+
+let age = 46;
+
+// age = age + 1;
+// age += 1;
+// age ++;
+// console.log(age++)           //----pre increment
+
+// console.log(++age)           //----post increment
+
+// console.log(age--)              //----pre increment
+
+// console.log(--age)              //----post increment
+  
